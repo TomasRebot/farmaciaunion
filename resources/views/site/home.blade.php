6b53aba70733647e0d03f22cf33359cf7b53f3f6
tomasrebot <tomas.devel>
2020-06-01 19:47:45 -0300

    fix home register no

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -68,13 +68,13 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/panel') }}">Panel administrador</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
+{{--                        @if (Route::has('register'))--}}
+{{--                            <a href="{{ route('register') }}">Register</a>--}}
+{{--                        @endif--}}
                     @endauth
                 </div>
             @endif
@@ -85,9 +85,11 @@
                 </div>
 
                 <div class="links">
-                    @foreach(GlobalConfig::networks() as $network)
+                    @forelse(GlobalConfig::networks() as $network)
                         <a href="{{$network['attr']}}">{{$network['label']}}</a>
-                    @endforeach
+                    @empty
+
+                    @endforelse
                 </div>
             </div>
         </div>
