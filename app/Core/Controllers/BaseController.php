55ad0fc765cfacd34bca45d5cd467cb88d0e91e0
tomasrebot <tomas.devel>
2020-06-01 21:24:02 -0300

    fix base controller

--- app/Core/Controllers/BaseController.php
+++ app/Core/Controllers/BaseController.php
@@ -1,9 +1,8 @@
 <?php
 namespace App\Core\Controllers;
-use App\Core\Traits\deleteHandlerTrait;
-use App\Exceptions\NonModelForSoftDeleteException;
+use App\Core\Traits\DeleteHandlerTrait;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+
 
 abstract class BaseController extends Controller
 {
