0c8005e0be0831cab2fc087720392a47a52f9070
tomasrebot <tomas.devel>
2020-06-02 12:05:09 -0300

    change factory to production

--- database/seeds/ClientSeeder.php
+++ database/seeds/ClientSeeder.php
@@ -15,20 +15,31 @@ class ClientSeeder extends Seeder
      */
     public function run()
     {
-
-
-        factory(User::class, 10)->create();
         $roleClient = Role::where('name', 'Cliente')->first()->id;
-        $already_have_role = UserRole::select('user_id')->get()->toArray();
-        $users_whitout_role = User::whereNotIn('id', $already_have_role)->select('id')->get()->toArray();
-        $final_insert = [];
-        foreach($users_whitout_role as $user){
-            array_push($final_insert,[
-                'user_id' => $user['id'],
+        for($i = 1; $i <= 10; $i++){
+            $idAdmin = DB::table('users')->insertGetId([
+                'name' => 'Cliente_'.$i,
+                'email' => 'cliente_'.$i.'@farmaciaunion.com',
+                'password' => bcrypt('123456'),
+            ]);
+            DB::table('user_roles')->insert([
+                'user_id' => $idAdmin,
                 'role_id' => $roleClient,
             ]);
         }
-        DB::table('user_roles')->insert($final_insert);
+
+//        factory(User::class, 10)->create();
+//        $roleClient = Role::where('name', 'Cliente')->first()->id;
+//        $already_have_role = UserRole::select('user_id')->get()->toArray();
+//        $users_whitout_role = User::whereNotIn('id', $already_have_role)->select('id')->get()->toArray();
+//        $final_insert = [];
+//        foreach($users_whitout_role as $user){
+//            array_push($final_insert,[
+//                'user_id' => $user['id'],
+//                'role_id' => $roleClient,
+//            ]);
+//        }
+//        DB::table('user_roles')->insert($final_insert);
 
 //
     }
